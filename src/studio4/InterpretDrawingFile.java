24bc7c7b21cd2a9fc19138fb9ed831e42d44a746
fedbo <fedbo@10.232.187.200>
2023-02-23 12:03:42 -0600

    wiehjegrnf

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,21 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape = in.next();
+		int R = in.nextInt();
+		int G = in.nextInt();
+		int B = in.nextInt();
+		boolean fill = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfwidth = in.nextDouble();
+		double halfheight = in.nextDouble();
+		StdDraw.setPenColor(R,G,B);
+		if (fill=true) {
+		StdDraw.filledRectangle(x, y, halfwidth, halfheight);
+		}
+		else {
+		StdDraw.rectangle(x, y, halfwidth, halfheight);
+		}
 	}
-}
+	}
